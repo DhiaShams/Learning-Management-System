64cc39084c960f3ebe1eade556352150f53d2df3
DhiaShams <dhiadhilu123@gmail.com>
2025-04-20 12:58:39 +0530

    Included relation between course and enrollment

--- models/course.js
+++ models/course.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Course.hasMany(models.Enrollment, { foreignKey: 'courseId' });
     }
   }
   Course.init({
