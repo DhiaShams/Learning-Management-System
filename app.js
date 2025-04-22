a86613fd9cdb2bd85878e38092f607fdfcab686e
DhiaShams <dhiadhilu123@gmail.com>
2025-04-22 21:08:21 +0530

    Added tiny csrf and cookie parser to app.js

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ const app = express();
 const authRoutes = require('./routes/authRoutes');//importing the login route
 const db = require('./models');  // This will auto-load your Sequelize setup
 
+const cookieParser = require('cookie-parser');
+const csrf = require('tiny-csrf');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser("ssh! some secret string"));
 app.use(express.json());
+app.use(csrf('your_super_secret_key_here_12345', ['POST', 'PUT', 'DELETE', 'PATCH']));
+
+// Example route: Sending CSRF token to client
+app.get('/csrf-token', (req, res) => {
+    res.json({ csrfToken: req.csrfToken() });
+});
+
+// Example route: Protected POST
+app.post('/example', (req, res) => {
+    res.json({ message: 'CSRF token matched, POST request accepted!' });
+});
+
 app.use('/api',authRoutes);
 
 const studentRoutes=require('./routes/student');
@@ -50,8 +67,4 @@ app.get('/', (req, res) => {
   res.send('LMS API is running!');
 });
 
-// Start server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server started on port ${PORT}`);
-});
+module.exports= app;
